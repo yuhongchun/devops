02ca9dd98f3771be59cfc4a65f6c6a7123810345
余洪春 <yuhongchun027@gmail.com>
2017-06-12 10:13:16 +0800

    Create client.py

--- ansible/client.py
+++ ansible/client.py
@@ -1,6 +1,6 @@
 import requests
 
-user_info = {'ips': ['223.87.1.210','119.44.18.156'], 'password': '123'}
-r = requests.post("http://202.85.220.118:5000/ansible/playbook/", data=user_info)
+user_info = {'ips': ['192.168.1.21','192.168.1.22'], 'password': '123'}
+r = requests.post("http://192.168.1.118:1234/ansible/playbook/", data=user_info)
 
 print r.text
