043743ce6de7914281a3f81d622ceac0dd5397e2
余洪春 <yuhongchun027@gmail.com>
2018-06-11 21:50:02 +0800

    Update initialv0.1.py

--- ansible/initialv0.1.py
+++ ansible/initialv0.1.py
@@ -1,5 +1,5 @@
-#encoding:utf-8
-#ansible 1.9.6
+#!/usr/bin/evn python
+#-*- encoding:utf-8 -*-
 import time
 import os
 import random
@@ -17,19 +17,14 @@ from tempfile import NamedTemporaryFile
 from rq import Queue
 from rq.job import Job
 from redis import Redis
+
 app = Flask(__name__)
-
-
 conn = Redis()
 q = Queue(connection=conn)  
-
-
 
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
-
-
 @app.route('/ansible/playbook/', methods=['POST'])
 def playbook():
     inventory = """
@@ -40,39 +35,22 @@ def playbook():
     """
     
     inventory_template = jinja2.Template(inventory)
-    #inst_ip  = [ i.encode('ascii') for i in request.form['ips']]
-    #ips = [  i for i in request.form.getlist("ips") ]
-    #inst_ip = request.form.get('ips')
-    #inst_ip = json.loads(request.form.getlist('ips'))
-    #inst_ip  = [ i.encode('ascii') for i in request.form.getlist('ips') ]
-    #print inst_ip
-    #print request.form
-    print request.get_data()
-    #data = request.get_data()
-    #inst_ip = request.form.getlist('ips[]')
-    #print data 
-    #inst_ip = data['ips']
-    #inst_ip = request.form.getlist('ips')
     data = json.loads(request.get_data())
     inst_ip = data["ips"]
-    print inst_ip
-    #inst_ip = ["192.168.1.3","192.168.1.4","192.168.1.5"]
     rendered_inventory = inventory_template.render({'hosts':inst_ip})
     hosts = NamedTemporaryFile(delete=False,suffix='tmp',dir='/tmp/ansible/')
     hosts.write(rendered_inventory)
     hosts.close()
+    '''
+    前端传递过来的Json数据，在Flask里面用了jinja2渲染成Ansible能识别的格式，并以临时文件的形式存在于/tmp/ansible/目录下
+    /tmp/ansible/目录可以提前建立
+    '''
     inventory = Inventory(hosts.name)
-    print inventory
-
     vars = {}
     stats = callbacks.AggregateStats()
     playbook_cb = callbacks.PlaybookCallbacks()
     runner_cb = callbacks.PlaybookRunnerCallbacks(stats)
-    # hosts = request.args.get('ip')
-    # task = request.args.get('playbook')
-    # vars['hosts'] = hosts
-    # play = task + '.yml'
-    pb = PlayBook(playbook='mytest.yml',callbacks=playbook_cb,runner_callbacks=runner_cb,stats=stats,inventory=inventory,extra_vars=vars)
+    pb = PlayBook(playbook='/root/ansible/mytest.yml',callbacks=playbook_cb,runner_callbacks=runner_cb,stats=stats,inventory=inventory,extra_vars=vars)
     job = q.enqueue_call(pb.run, result_ttl=5000, timeout=2000)
     jid = job.get_id()
     if jid:
@@ -92,8 +70,6 @@ def get_results(job_key):
       ret = {'status':'waiting'}
   elif job.is_failed:
       ret = {'status': 'failed'}
-
   return json.dumps(ret), 200
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=1234)
+    app.run(host='0.0.0.0',port=5000)
