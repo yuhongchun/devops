3dca22f38a03cba882a39032a56f0294f02ec23c
余洪春 <yuhongchun027@gmail.com>
2018-06-11 11:42:57 +0800

    Update client.py

--- ansible/client.py
+++ ansible/client.py
@@ -1,6 +1,7 @@
 import requests
 
+headers =  {'Content-Type': 'application/json'}
 user_info = {'ips': ['192.168.1.21','192.168.1.22'], 'password': '123'}
-r = requests.post("http://192.168.1.118:1234/ansible/playbook/", data=user_info)
+r = requests.post("http://192.168.1.118:1234/ansible/playbook/", headers=headers,data=user_info)
 
 print r.text
