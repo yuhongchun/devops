0464bd7ca18b56a8c6bab7096b365d958bbfa3f9
Administrator <yuhongchun027@163.com>
2018-12-15 19:50:31 +0800

    update

---
+++ 《DevOps和自动化运维实践》第3章/3.7.1/1.py
@@ -0,0 +1,35 @@
+# -*- coding:utf8 -*-
+from flask import Flask, request, jsonify
+from flask import jsonify
+app = Flask(__name__)
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+@app.route('/hello/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+if __name__ == '__main__':
+    app.run(port=5001,debug=True)
