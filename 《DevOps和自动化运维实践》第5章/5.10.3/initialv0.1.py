890a0795fc4a615d1618a6b194a26ef655c641af
Administrator <yuhongchun027@163.com>
2018-12-17 10:27:34 +0800

    update

--- 《DevOps和自动化运维实践》第5章/5.10.3/initialv0.1.py
+++ 《DevOps和自动化运维实践》第5章/5.10.3/initialv0.1.py
@@ -81,17 +81,4 @@ def get_results(job_key):
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
-work.py文件内容如下所示：
-# ！/usr/bin/evn python
-import os
-import redis
-from rq import Worker, Queue, Connection
 
-listen = ['default']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-conn = redis.from_url(redis_url)
-
-if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(list(map(Queue, listen)))
-        worker.work()
